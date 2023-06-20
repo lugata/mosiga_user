ac64ec895ff6f7f5f956b7c47b74b0672b1dabab
lugata <ata040602@gmail.com>
2023-06-20 22:20:41 +0700

    gabut

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:mosiga_users/screens/main_page.dart';
 import 'package:mosiga_users/screens/register_screen.dart';
 import 'package:mosiga_users/theme/theme.dart';
 
