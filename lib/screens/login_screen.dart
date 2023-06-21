0adaf70c36e6c93a3400ea3305a9307e2f8d6e79
lugata_ata <ata040602@gmail.com>
2023-06-21 10:36:38 +0700

    login_screen

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'register_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(),
+    );
+  }
+}
