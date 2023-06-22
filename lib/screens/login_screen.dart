4695c0e78d4384c3c8949366974bcde2a90dcd94
lugata <ata040602@gmail.com>
2023-06-22 17:23:52 +0700

    Fix Bug + CheckPoint

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -34,7 +34,7 @@ class _LoginScreenState extends State<LoginScreen> {
 
         await Fluttertoast.showToast(msg: "Berhasil Masuk");
         Navigator.pushReplacement(
-            context, MaterialPageRoute(builder: (c) => MainScreen()));
+            context, MaterialPageRoute(builder: (c) => const MainScreen()));
       }).catchError((error) {
         // Tangkap dan tangani kesalahan yang terjadi
         Fluttertoast.showToast(msg: "Email atau Password Salah");
